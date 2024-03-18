f4bb208ab342804e7c454321c6c5442327419ec8
Liqutch <liqutch@gmail.com>
2024-03-18 18:48:36 +0300

    Renewed code

--- main.py
+++ main.py
@@ -1,100 +1,192 @@
-try:
-  import os
-  import time
-  import json
-  import zlib
-  import base64
-  import logging
-  import requests
-  import coloredlogs
-except ImportError as e:
-  print(f'{e.name} modülü yüklü değil. Lütfen programı çalıştırmadan önce "packages.bat" ile gerekli modülleri yükleyin.')
-  time.sleep(5)
-  exit()
-  
-os.system('cls')
-os.system('TITLE Fortnite.GG Locker by Liqutch')
+import os
+import sys
+import json
+import zlib
+import base64
+import aiohttp
+import asyncio
+import inquirer
+import platform
+import pyperclip
+import webbrowser
+import coloredlogs
+
+
+__version__ = "2.0"
+SWITCH_TOKEN = "OThmN2U0MmMyZTNhNGY4NmE3NGViNDNmYmI0MWVkMzk6MGEyNDQ5YTItMDAxYS00NTFlLWFmZWMtM2U4MTI5MDFjNGQ3"
+
+
+os.system('cls' if sys.platform.startswith('win') else 'clear')
+os.system(f'TITLE FNGG-LockerGenerator v{__version__} by Liqutch')
+
 coloredlogs.logging.basicConfig(level=coloredlogs.logging.INFO)
 log = coloredlogs.logging.getLogger(__name__)
 coloredlogs.install(fmt="[%(asctime)s][%(levelname)s] %(message)s", datefmt="%H:%M:%S", logger=log)
 
-if not os.path.isfile('account.json'):       
-  log.error('"account.json" dosyası bulunamadı, program kapatılıyor...')
-  time.sleep(5)
-  exit()
 
-try:
-  with open("account.json", encoding="utf-8") as f:
-    data = json.load(f)
-    account_id = data["account_id"]
-    device_id = data["device_id"]
-    secret = data["secret"]
-except:
-  log.error("Hesap bilgilerini alırken bir sorun oluştu. Lütfen dosyanın düzgün yapılandırıldığından emin olun.")
-  time.sleep(5)
-  exit()
+class EpicAccount:
+    def __init__(self, data: dict = {}) -> None:
+        self.raw = data
 
-def generateToken():
-  url = "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token"
-  body = f"grant_type=device_auth&account_id={account_id}&device_id={device_id}&secret={secret}"
-  headers = {
-    "Authorization":
-    "basic M2Y2OWU1NmM3NjQ5NDkyYzhjYzI5ZjFhZjA4YThhMTI6YjUxZWU5Y2IxMjIzNGY1MGE2OWVmYTY3ZWY1MzgxMmU=",
-    "Content-Type": "application/x-www-form-urlencoded"
-  }
-  response = requests.post(url, data=body, headers=headers)
-  if response.status_code == 400:
-    log.error("Token oluşturulamadı. Lütfen bilgilerinizi kontrol edin.")
-    time.sleep(5)
-    exit()
-  if response.status_code == 200:
-    log.info("Token başarıyla oluşturuldu.")
-    return response.json()["access_token"]
-token = generateToken()
+        self.access_token = data.get("access_token", "")
+        self.display_name = data.get("displayName", "")
+        self.account_id = data.get("account_id", "")
+    
+    async def get_profile(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.request(
+                method="POST",
+                url="https://fngw-mcp-gc-livefn.ol.epicgames.com"
+                f"/fortnite/api/game/v2/profile/{self.account_id}/client/QueryProfile?profileId=athena&rvn=-1",
+                headers={"Authorization": f"bearer {self.access_token}", "Content-Type": "application/json"},
+                data=json.dumps({})
+            ) as request:
+                if request.status == 200:
+                    return await request.json()
+                else:
+                    data = await request.json()
+                    log.info("There was a problem while getting the profile.")
+                    log.info("{}/{}/{}\n".format(request.status, data["numericErrorCode"], data["errorCode"]))
+                    log.info(f"ProfileExtractor v{__version__} will close in 5 seconds. Try again later.")
+                    await asyncio.sleep(5)
+                    sys.exit()
 
-headers = {
-    "Authorization": f"Bearer {token}",
-    "Content-Type": "application/json"
-}
-request = requests.post(f"https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/{account_id}/client/QueryProfile?profileId=athena", data="{}", headers=headers)
-if request.status_code == 200:
-  log.info("Profil bilgileri başarıyla alındı.")
-  data = request.json()
-  athena_creation_date = data["profileChanges"][0]["profile"]["created"]
-  account_items = data["profileChanges"][0]["profile"]["items"]
-  cosmetics = ["AthenaCharacter", "AthenaBackpack", "AthenaPickaxe", "AthenaDance", "AthenaGlider", "AthenaItemWrap", "AthenaLoadingScreen", "AthenaMusicPack", "AthenaSkyDiveContrail", "BannerToken"]
 
-  locker = []
-  for item in account_items:
-      if data["profileChanges"][0]["profile"]["items"][item]["templateId"].split(":")[0] in cosmetics:
-        locker.append(data["profileChanges"][0]["profile"]["items"][item]["templateId"].split(":")[1])
-  
-  items = []
-  response = requests.get('https://fortnite.gg/api/items.json')
-  log.info("Dolap içerikleri Fortnite.GG ile eşleştiriliyor...")
-  for lockeritems in locker:
-    for itemlist in response.json():
-      if itemlist.lower() == lockeritems:
-        items.append(itemlist)
-  try:
-    fngg_items = requests.get('https://fortnite.gg/api/items.json').json()
-    ints = list(map(lambda it: int(fngg_items[it]), items))
-    ints.sort()
+class Generator:
+    def __init__(self) -> None:
+        self.http: aiohttp.ClientSession
 
-    diff = list(map(lambda e: str(e[1] - ints[e[0] - 1]) if e[0] > 0 else str(e[1]), enumerate(ints)))
-    compress = zlib.compressobj(level=-1, method=zlib.DEFLATED, wbits=-9,
-    memLevel=zlib.DEF_MEM_LEVEL, strategy=zlib.Z_DEFAULT_STRATEGY)
-    compressed = compress.compress(f"{athena_creation_date},{','.join(diff)}".encode())
-    compressed += compress.flush()
+        self.access_token = ""
+        self.user_agent = f"FNGG-LockerGenerator/{__version__} {platform.system()}/{platform.version()}"
+        
+    async def start(self) -> None:
+        self.http = aiohttp.ClientSession(headers={"User-Agent": self.user_agent})
 
-    encoded = base64.urlsafe_b64encode(compressed).decode().rstrip("=")
-    url = f"https://fortnite.gg/my-locker?items={encoded}"
-  except:
-    log.error("Eşleştirme sırasında bir hata oluştu, lütfen tekrar deneyin veya destek alın.")
-  log.info(f"Fortnite.GG bağlantısı başarıyla oluşturuldu:\n{url}")
-  os.startfile(url)
-  input()
-else:
-  log.error(f"Profil bilgileri alınırken bir hata oluştu. [{request.status_code}]")
-  time.sleep(5)
-  exit()
+        self.access_token = await self.get_access_token()
+
+        log.info("Opening device code link in a new tab...")
+        device_code = await self.create_device_code()
+        webbrowser.open(device_code[0], new=1)
+
+        account = await self.wait_for_device_code_completion(code=device_code[1])
+        data = await account.get_profile()
+
+        os.system('cls' if sys.platform.startswith('win') else 'clear')
+        log.info(f"Logged in as: {account.display_name}\n")
+
+        choice = [inquirer.Confirm('start', message="Do you want to start the process?",default=True)]
+        answers = inquirer.prompt(choice)
+
+        os.system('cls' if sys.platform.startswith('win') else 'clear')
+        if not answers['start']:
+            log.info(f'Closing FNGG-LockerGenerator v{__version__}...')
+            await asyncio.sleep(1)
+            sys.exit()
+
+        account_items = data["profileChanges"][0]["profile"]["items"]
+        athena_creation_date = data["profileChanges"][0]["profile"]["created"]
+
+        log.info("Generating the locker...")
+        all_items = list(map(lambda item: account_items[item]["templateId"].split(":")[1], account_items))
+        
+        locker = []
+        fngg_items = await self.get_fngg_items()
+        fngg_items_lowercase = list(map(lambda x: x.lower(), fngg_items.keys()))
+
+        for item in all_items: 
+            if item in fngg_items_lowercase:
+                original_id = list(fngg_items.keys())[fngg_items_lowercase.index(item.lower())]
+                locker.append(original_id)
+
+        ints = list(map(lambda it: int(fngg_items[it]), locker))
+        ints.sort()
+
+        diff = list(map(lambda e: str(e[1] - ints[e[0] - 1]) if e[0] > 0 else str(e[1]), enumerate(ints)))
+        compress = zlib.compressobj(
+            level=-1,
+            method=zlib.DEFLATED,
+            wbits=-9,
+            memLevel=zlib.DEF_MEM_LEVEL,
+            strategy=zlib.Z_DEFAULT_STRATEGY
+        )
+        compressed = compress.compress(f"{athena_creation_date},{','.join(diff)}".encode())
+        compressed += compress.flush()
+
+        encoded = base64.urlsafe_b64encode(compressed).decode().rstrip("=")
+        
+        url = f"https://fortnite.gg/my-locker?items={encoded}"
+        os.startfile(url)
+
+        log.info("Link successfully copied to the clipboard. Press enter to close the program.")
+        pyperclip.copy(url)
+        
+        input(), sys.exit()
+
+    async def get_fngg_items(self) -> dict:
+        async with self.http.request(
+            method="GET",
+            url="https://fortnite.gg/api/items.json",
+        ) as request:
+            return await request.json()
+    
+    async def get_access_token(self) -> str:
+        async with self.http.request(
+            method="POST",
+            url="https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token",
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": f"basic {SWITCH_TOKEN}",
+            },
+            data={
+                "grant_type": "client_credentials",
+            },
+        ) as request:
+            data = await request.json()
+
+        return data["access_token"]
+    
+    async def create_device_code(self) -> tuple:
+        async with self.http.request(
+            method="POST",
+            url="https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/deviceAuthorization",
+            headers={
+                "Authorization": f"bearer {self.access_token}",
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        ) as request:
+            data = await request.json()
+
+        return data["verification_uri_complete"], data["device_code"]
+
+    async def wait_for_device_code_completion(self, code: str) -> EpicAccount:
+        os.system('cls' if sys.platform.startswith('win') else 'clear')
+        log.info("Waiting for authentication...")
+
+        while True:
+            async with self.http.request(
+                method="POST",
+                url="https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/token",
+                headers={
+                    "Authorization": f"basic {SWITCH_TOKEN}",
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                data={"grant_type": "device_code", "device_code": code},
+            ) as request:
+                if request.status == 200:
+                    auth_data = await request.json()
+                    break
+                else:
+                    pass
+
+                await asyncio.sleep(5)
+                
+        return EpicAccount(data=auth_data)
+
+    def run(self) -> None:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.start())
+        loop.run_forever()
+
+
+ext = Generator()
+ext.run()
--- packages.bat
+++ packages.bat
@@ -6,7 +6,7 @@ python --version >nul 2>&1
 if %errorlevel% neq 0 (
   echo Python is not installed. Installing Python...
   REM Download and install Python from official website
-  powershell -Command "& {Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe' -OutFile 'python-installer.exe'}"
+  powershell -Command "& {Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.10.11/python-3.10.11-amd64.exe' -OutFile 'python-installer.exe'}"
   python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 )
 
